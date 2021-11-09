c46be9917a2dc722e235cf62a00f89ca69908a71
Francisco García <francoffee57@gmail.com>
2021-11-08 23:09:57 -0600

    Corregí los colores y modifiqué el texto

--- SRC/main.js
+++ SRC/main.js
@@ -3,13 +3,14 @@ misRedes = [
     {name : 'Instagram', url: 'https://instagram.com/frangardev'},
     {name : 'GitHub', url: 'https://github.com/frangardev'},
     {name : 'Linkedin', url: 'https://www.linkedin.com/in/frangardev/'},
+    {name : 'Platzi', url: 'https://platzi.com/p/frangardev/'},
 ]
 
 redesConteiner = document.getElementById('redes')
 
 nodes = []
 misRedes.forEach(({name, url}) => {
-    const element = `<li class="redes__li"><a href="${url}" target="_blank" class="button"><span class="icon"></span>${name}</a></li>`;
+    const element = `<li class="redes__li"><a href="${url}" target="_blank" class="button"><span class="icon"></span><span class="redes__name">${name}</span></a></li>`;
     nodes.push(element)
 })
 
