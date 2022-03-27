9806ef17f01b94f775464b2c1eb81eb263528228
Francisco <frangardev@outlook.com>
2022-03-27 14:05:53 -0600

    new links

--- SRC/index.js
+++ SRC/index.js
@@ -6,6 +6,8 @@ const misRedes = [
     {name : 'GitHub', url: 'https://github.com/frangardev'},
     {name : 'Instagram', url: 'https://instagram.com/frangardev'},
     {name : 'Platzi', url: 'https://platzi.com/p/frangardev/'},
+    {name : 'Behance', url: 'https://www.behance.net/frangardev'},
+    {name : 'Pinterest', url: 'https://www.pinterest.com.mx/frangardev/'},
 ]
 
 const redesConteiner = document.getElementById('redes');
